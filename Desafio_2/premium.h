3568dfd33a7f6d58e469e1219b5b88c1c8619ce6
Angel Espinosa David <angel.espinosa@udea.edu.co>
2025-10-22 23:45:02 -0500

    Desarrollando la clases. No esta completa la logica.

--- Desafio_2/premium.h
+++ Desafio_2/premium.h
@@ -1,10 +1,19 @@
 #ifndef PREMIUM_H
 #define PREMIUM_H
+#include "usuario.h"
 
-class Premium
+
+class Premium:public Usuario
 {
+    int cantListas;
+    string listas;
 public:
     Premium();
+    Premium(string, string, string, string, string, int, int, int, string,int, string);
+
+    void seguirListas();
+    void reproducir();
+
 };
 
 #endif // PREMIUM_H
